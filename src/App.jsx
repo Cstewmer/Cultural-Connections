90869b011bf88a9794a6e82fd4e7dc8b432f92aa
Charles Stewart Merriam <159678108+Cstewmer@users.noreply.github.com>
2025-06-16 04:01:19 +0000

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Cultural-Connections">
       <div className="navbar">
         <Link to="/" className="navbar-title">Cultural Connections</Link>
         <div className="navbar-links">
